0417acbb1407b2235ea6bae9138a0306dec1a613
Samrwitt <samrawitkta@gmail.com>
2024-05-17 15:31:25 +0300

    Class Exercise 2

--- Class-Ex2.kt
+++ Class-Ex2.kt
@@ -1,5 +1,4 @@
 class Triangle(private val side1: Double, private val side2: Double, private val side3: Double) {
-
     fun determineType(): String {
         return when {
             side1 == side2 && side2 == side3 -> "Equilateral"
@@ -7,4 +6,15 @@ class Triangle(private val side1: Double, private val side2: Double, private val
             else -> "Scalene"
         }
     }
+}
+
+fun main() {
+    val triangle1 = Triangle(3.0, 3.0, 3.0)
+    println("Triangle 1: ${triangle1.determineType()}")
+
+    val triangle2 = Triangle(4.0, 4.0, 5.0)
+    println("Triangle 2: ${triangle2.determineType()}")
+
+    val triangle3 = Triangle(3.0, 4.0, 5.0)
+    println("Triangle 3: ${triangle3.determineType()}")
 }
