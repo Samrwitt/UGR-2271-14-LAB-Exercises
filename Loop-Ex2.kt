6d02d1f8ba6bdf8cea378358d30c72e192171d59
Samrwitt <samrawitkta@gmail.com>
2024-05-17 15:41:06 +0300

    loop exercise 2

--- Loop-Ex2.kt
+++ Loop-Ex2.kt
@@ -1,11 +1,32 @@
+fun isPrime(number: Int): Boolean {
+    if (number <= 1) {
+        return false
+    }
+    var divisor = 2
+    while (divisor * divisor <= number) {
+        if (number % divisor == 0) {
+            return false
+        }
+        divisor++
+    }
+    return true
+}
+
 fun findPrimesInRange(start: Int, end: Int): List<Int> {
     val primes = mutableListOf<Int>()
-    var num = start
-    while (num <= end) {
-        if (isPrime(num)) {
-            primes.add(num)
+    var currentNumber = start
+    while (currentNumber <= end) {
+        if (isPrime(currentNumber)) {
+            primes.add(currentNumber)
         }
-        num++
+        currentNumber++
     }
     return primes
+}
+
+fun main() {
+    val start = 1
+    val end = 50
+    val primes = findPrimesInRange(start, end)
+    println("Prime numbers between $start and $end: $primes")
 }
